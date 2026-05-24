6d0046940e9f56d3c143e139eccc08c3314e32df
pitlaraya <visual.pdr@gmail.com>
2026-05-23 22:54:37 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,19 @@
 const CACHE = 'habit-tracker-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = [
+  '/Life-Project/',
+  '/Life-Project/index.html',
+  '/Life-Project/manifest.json',
+  '/Life-Project/icon-192.png',
+  '/Life-Project/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
